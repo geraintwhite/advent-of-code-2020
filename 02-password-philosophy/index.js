d99e944cc60905b68a403e568c214104c249c20b
Geraint White <geraintwhite@gmail.com>
2020-12-12 23:45:36 +0000

    Day 2 part 2

--- 02-password-philosophy/index.js
+++ 02-password-philosophy/index.js
@@ -2,20 +2,34 @@ const fs = require('fs');
 
 const data = fs.readFileSync('data.txt').toString();
 
+const parseLine = (line) => line.match(/(\d+)-(\d+) (\w): (\w+)/);
+
 const validatePassword = (password) => {
-  const [_, a, b, c, d] = password.match(/(\d+)-(\d+) (\w): (\w+)/);
+  const [_, a, b, c, d] = parseLine(password);
   const count = d.split(c).length - 1;
   return count >= Number(a) && count <= Number(b);
 };
 
-const countValidPasswords = (passwords) => {
-  return passwords.filter(validatePassword).length;
+const validatePassword2 = (password) => {
+  const [_, a, b, c, d] = parseLine(password);
+  const matches = [d[Number(a) - 1] === c, d[Number(b) - 1] === c];
+  return matches.filter(Boolean).length === 1;
 };
 
-console.log(countValidPasswords([
+const countValidPasswords = (passwords, validator) => {
+  return passwords.filter(validator).length;
+};
+
+const lines = data.split('\n').filter(Boolean);
+
+const testData = [
   '1-3 a: abcde',
   '1-3 b: cdefg',
   '2-9 c: ccccccccc'
-]));
+];
 
-console.log(countValidPasswords(data.split('\n').filter(Boolean)));
+console.log(countValidPasswords(testData, validatePassword));
+
+console.log(countValidPasswords(lines, validatePassword));
+
+console.log(countValidPasswords(lines, validatePassword2));
